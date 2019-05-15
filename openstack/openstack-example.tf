31e4782f1e57722967d794f119d122f6593dec31
Guillermo Alvarado <guillermoalvarado89@gmail.com>
2019-05-15 17:23:28 -0500

    MySQL example

---
+++ openstack/openstack-example.tf
@@ -0,0 +1,25 @@
+# Configure the OpenStack Provider
+provider "openstack" {
+  user_name   = "admin"
+  tenant_name = "admin"
+  password    = "w3h58h3ooq"
+  auth_url    = "http://controller01:5000/v3"
+  region      = "RegionOne"
+}
+
+# Create a RHEL server
+resource "openstack_compute_instance_v2" "basic" {
+  name            = "vm_from_terraform"
+  image_id        = "765887bd-2635-4c2e-9feb-248a1b7707ea"
+  flavor_id       = "c98478b4-2d58-42f6-940e-15bdea5aa74f"
+  
+  metadata = {
+    this = "that"
+  }
+
+  network {
+    name = "Some_Network"
+  }
+
+
+}
