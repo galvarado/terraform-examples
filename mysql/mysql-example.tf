d03c0aad6c5cb05de6dbd5654570e431bbb722f3
Guillermo Alvarado <guillermoalvarado89@gmail.com>
2019-05-16 10:29:46 -0500

    Adding some comments to autoexplain args

--- mysql/mysql-example.tf
+++ mysql/mysql-example.tf
@@ -1,18 +1,18 @@
 provider "mysql" {
-  endpoint = "localhost:3306"
-  username = "root"   #Some user with privileges
-  password = "Pb5c2.<:-Gf7vc4M"  #Some strong pass
+  endpoint = "localhost:3306"  # MySQL Endpoint
+  username = "root"   # Some existing ser with privileges
+  password = "Pb5c2.<:-Gf7vc4M"  # User pass
 }
 
 # Crear base de datos
-resource "mysql_database" "proyecto" {
-  name = "proyecto"
+resource "mysql_database" "some_db" {
+  name = "some_db_name"  #Database to be created
 }
 
 # Crear usuario
 resource "mysql_user" "demo" {
-  user     = "demo"  #Some user
-  host     = "localhost"
-  password = "Xd5c2.<:-Gf7vc5M"    #Some strong pass
+  user     = "demo"  # User to be created
+  host     = "localhost"  #host where database will be created
+  password = "Xd5c2.<:-Gf7vc5M"    # Some strong pass for demo user
 }
 
